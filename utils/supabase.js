cdd9fddc72f0175395f6fce9bd5311ace44f24fb
pblgllgs <pbl.gllgs@gmail.com>
2022-08-20 17:51:09 -0400

    update supabases url, anon

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_2;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_2;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
